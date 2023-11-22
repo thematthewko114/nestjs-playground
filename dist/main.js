ff95efe72e7c78bc3827799f245bd243fd6680c1
Matthew Ko <iammatthewko114@gmail.com>
2023-11-22 14:52:22 +0800

    recommit

--- dist/main.js
+++ dist/main.js
@@ -4,7 +4,7 @@ const core_1 = require("@nestjs/core");
 const modules_1 = require("./modules");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(modules_1.AppModule);
-    await app.listen(4000);
+    await app.listen(3000);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
